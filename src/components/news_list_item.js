c9343539a45da68a746336d6e534bf235d871f68
Patricio <patricioarce94@gmail.com>
2020-08-13 17:44:38 -0300

    lasfl

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -4,12 +4,12 @@ import classes from '../css/styles.css'
 const NewsItem = ({item}) =>{
 
     return (
-        <div className={classes.new_item}>
+        <div className={classes.news_item}>
             <h3>{item.title}</h3>
             <div>{item.feed}</div>
         </div>
     )
-}
+} 
 
 export default NewsItem;
 
